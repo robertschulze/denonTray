2f7e3f3f28a15f43609bb13c565d224de134abb3
Robert Schulze <robert@guitaronline.de>
2020-03-08 02:31:07 +0100

    black formatting

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='denontray',
-    version='0.1',
-    description='System tray remote-control tool for Denon AVR receivers',
-    long_description='System tray remote-control tool for Denon AVR receivers',
-    url='https://github.com/robertschulze/denonTray',
-    author='Robert Schulze',
-    author_email='robert@guitaronline.de',
-    license='MIT',
+    name="denontray",
+    version="0.1",
+    description="System tray remote-control tool for Denon AVR receivers",
+    long_description="System tray remote-control tool for Denon AVR receivers",
+    url="https://github.com/robertschulze/denonTray",
+    author="Robert Schulze",
+    author_email="robert@guitaronline.de",
+    license="MIT",
     packages=find_packages(),
-    install_requires=['denonavr', 'infi.systray', 'pyyaml', 'infi'],
-    tests_require=['tox'],
-    platforms=['any'],
+    install_requires=["denonavr", "infi.systray", "pyyaml", "infi"],
+    tests_require=["tox"],
+    platforms=["any"],
     zip_safe=False,
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -27,4 +27,5 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-    ])
+    ],
+)
